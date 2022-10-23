b3a38b7295a4ea89ea5c9b48b41570bbe0f3be68
Greg <litm47@gmail.com>
2022-10-23 16:06:37 -0400

    added users model

---
+++ models/Users.js
@@ -0,0 +1,49 @@
+const {Model, Datatypes} = require('sequelize');
+
+const bcrypt = require('bcrypt');
+
+const sequelize = require('../config/connection');
+
+class Users extends Model {
+    checkPassword(loginPass){
+        return bcrypt.compareSync(loginPass, this.password);
+    }
+}
+
+Users.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser;
+            },
+            beforeUpdate: async (updateUser) => {
+                updateUser.password = await bcrypt.hash(updateUser.password, 10);
+                
+            },
+        },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'users',
+    }
+);
+
+module.exports = Users;
