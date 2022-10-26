2254de8fb3ad14f01582a49b7afd124bf5b523cb
Greg <litm47@gmail.com>
2022-10-26 17:11:11 -0400

    Added home page route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,27 @@ const router = require('express').Router();
 
 const { Users, Posts, Comments } = require('../models');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const currentPosts = await Users.findAll({
+            attributes: {
+                exclude: [
+                    'password',
+                ],
+            },
+            include: [{model: Posts}]
+        });
+        const posts = currentPosts.map((post) => post.get({ plain: true }))
+        const loggedIn = req.session.logged_in
+        res.render('home',{
+            posts,
+            loggedIn
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err)
+    }
+});
 
 
 
